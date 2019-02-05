c14fc0255db788de784e2b3f4725cb847ed1932f
Tufik Chediak Sanchez <tufik2@gmail.com>
2019-02-04 20:45:24 -0500

    Fist implementation

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Absolute path to the media folder e.g. C:/path/to/media/
+$rootFolder = __DIR__."/js/fileupload/server/php/files/";
+
+// Absolute URL to the media file, e.g. http://domain.com/media/
+$rootURL = "http://localhost/projects/tgolden_group/filemanager/www/js/fileupload/server/php/files/";
+
+// URL to the php file to process the upload
+$phpPath = "js/fileupload/server/php/";
